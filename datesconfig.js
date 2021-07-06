758aba12729e33a063e812af529d9598ae0aeb3d
PedroLeon <pedro1.leon1@gmail.com>
2021-07-05 21:44:20 -0400

    Add mon

--- datesconfig.js
+++ datesconfig.js
@@ -3,7 +3,7 @@ class DatesConfig {
     this.departure = {
       minDate: 0,
       maxDate: 365,
-      validDaysOfWeek: [],
+      validDaysOfWeek: [mon],
       includedDates: [],
       excludedDates: [],
       excludedDateRanges: [],
